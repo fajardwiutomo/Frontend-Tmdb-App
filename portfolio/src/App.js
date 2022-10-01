ea0604f68bbf9c0dea4bc89a3df521ae62413091
fajardwiutomo <fajardwi576@gmail.com>
2022-10-01 12:55:12 +0700

    first

---
+++ portfolio/src/App.js
@@ -0,0 +1,35 @@
+import { Home } from "./pages/home/Home";
+import { Login } from "./pages/login/Login";
+import { Register } from "./pages/register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./auth/Auth";
+import { useEffect, useState } from "react";
+import { Detail } from "./pages/detail/Detail";
+
+function App() {
+  const [signedIn, setSignin] = useState('')
+  useEffect(()=> {
+    const signedIn = localStorage.getItem("access_token")
+    setSignin(signedIn)
+  },[])
+
+ 
+
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Auth signedIn={signedIn}/>}>
+            <Route path="/home" element={<Home />} exact/>
+          </Route>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/detail" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
